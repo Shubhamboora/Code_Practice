0321254d8624a3b219737a88ece2041293705312
Shubham <shubhamboora0@gmail.com>
2022-08-16 01:05:35 +0530

    Word Ladder commit(Hard category)

---
+++ src/dailyChalanges/wordLadder2.java
@@ -0,0 +1,91 @@
+package dailyChalanges;
+
+import java.util.*;
+
+public class wordLadder2 {
+    public static void main(String[] args) {
+        //String[] arr = {"aaaaa","caaaa","cbaaa","daaaa","dbaaa","eaaaa","ebaaa","faaaa","fbaaa","gaaaa","gbaaa","haaaa","hbaaa","iaaaa","ibaaa","jaaaa","jbaaa","kaaaa","kbaaa","laaaa","lbaaa","maaaa","mbaaa","naaaa","nbaaa","oaaaa","obaaa","paaaa","pbaaa","bbaaa","bbcaa","bbcba","bbdaa","bbdba","bbeaa","bbeba","bbfaa","bbfba","bbgaa","bbgba","bbhaa","bbhba","bbiaa","bbiba","bbjaa","bbjba","bbkaa","bbkba","bblaa","bblba","bbmaa","bbmba","bbnaa","bbnba","bboaa","bboba","bbpaa","bbpba","bbbba","abbba","acbba","dbbba","dcbba","ebbba","ecbba","fbbba","fcbba","gbbba","gcbba","hbbba","hcbba","ibbba","icbba","jbbba","jcbba","kbbba","kcbba","lbbba","lcbba","mbbba","mcbba","nbbba","ncbba","obbba","ocbba","pbbba","pcbba","ccbba","ccaba","ccaca","ccdba","ccdca","cceba","cceca","ccfba","ccfca","ccgba","ccgca","cchba","cchca","cciba","ccica","ccjba","ccjca","cckba","cckca","cclba","cclca","ccmba","ccmca","ccnba","ccnca","ccoba","ccoca","ccpba","ccpca","cccca","accca","adcca","bccca","bdcca","eccca","edcca","fccca","fdcca","gccca","gdcca","hccca","hdcca","iccca","idcca","jccca","jdcca","kccca","kdcca","lccca","ldcca","mccca","mdcca","nccca","ndcca","occca","odcca","pccca","pdcca","ddcca","ddaca","ddada","ddbca","ddbda","ddeca","ddeda","ddfca","ddfda","ddgca","ddgda","ddhca","ddhda","ddica","ddida","ddjca","ddjda","ddkca","ddkda","ddlca","ddlda","ddmca","ddmda","ddnca","ddnda","ddoca","ddoda","ddpca","ddpda","dddda","addda","aedda","bddda","bedda","cddda","cedda","fddda","fedda","gddda","gedda","hddda","hedda","iddda","iedda","jddda","jedda","kddda","kedda","lddda","ledda","mddda","medda","nddda","nedda","oddda","oedda","pddda","pedda","eedda","eeada","eeaea","eebda","eebea","eecda","eecea","eefda","eefea","eegda","eegea","eehda","eehea","eeida","eeiea","eejda","eejea","eekda","eekea","eelda","eelea","eemda","eemea","eenda","eenea","eeoda","eeoea","eepda","eepea","eeeea","ggggg","agggg","ahggg","bgggg","bhggg","cgggg","chggg","dgggg","dhggg","egggg","ehggg","fgggg","fhggg","igggg","ihggg","jgggg","jhggg","kgggg","khggg","lgggg","lhggg","mgggg","mhggg","ngggg","nhggg","ogggg","ohggg","pgggg","phggg","hhggg","hhagg","hhahg","hhbgg","hhbhg","hhcgg","hhchg","hhdgg","hhdhg","hhegg","hhehg","hhfgg","hhfhg","hhigg","hhihg","hhjgg","hhjhg","hhkgg","hhkhg","hhlgg","hhlhg","hhmgg","hhmhg","hhngg","hhnhg","hhogg","hhohg","hhpgg","hhphg","hhhhg","ahhhg","aihhg","bhhhg","bihhg","chhhg","cihhg","dhhhg","dihhg","ehhhg","eihhg","fhhhg","fihhg","ghhhg","gihhg","jhhhg","jihhg","khhhg","kihhg","lhhhg","lihhg","mhhhg","mihhg","nhhhg","nihhg","ohhhg","oihhg","phhhg","pihhg","iihhg","iiahg","iiaig","iibhg","iibig","iichg","iicig","iidhg","iidig","iiehg","iieig","iifhg","iifig","iighg","iigig","iijhg","iijig","iikhg","iikig","iilhg","iilig","iimhg","iimig","iinhg","iinig","iiohg","iioig","iiphg","iipig","iiiig","aiiig","ajiig","biiig","bjiig","ciiig","cjiig","diiig","djiig","eiiig","ejiig","fiiig","fjiig","giiig","gjiig","hiiig","hjiig","kiiig","kjiig","liiig","ljiig","miiig","mjiig","niiig","njiig","oiiig","ojiig","piiig","pjiig","jjiig","jjaig","jjajg","jjbig","jjbjg","jjcig","jjcjg","jjdig","jjdjg","jjeig","jjejg","jjfig","jjfjg","jjgig","jjgjg","jjhig","jjhjg","jjkig","jjkjg","jjlig","jjljg","jjmig","jjmjg","jjnig","jjnjg","jjoig","jjojg","jjpig","jjpjg","jjjjg","ajjjg","akjjg","bjjjg","bkjjg","cjjjg","ckjjg","djjjg","dkjjg","ejjjg","ekjjg","fjjjg","fkjjg","gjjjg","gkjjg","hjjjg","hkjjg","ijjjg","ikjjg","ljjjg","lkjjg","mjjjg","mkjjg","njjjg","nkjjg","ojjjg","okjjg","pjjjg","pkjjg","kkjjg","kkajg","kkakg","kkbjg","kkbkg","kkcjg","kkckg","kkdjg","kkdkg","kkejg","kkekg","kkfjg","kkfkg","kkgjg","kkgkg","kkhjg","kkhkg","kkijg","kkikg","kkljg","kklkg","kkmjg","kkmkg","kknjg","kknkg","kkojg","kkokg","kkpjg","kkpkg","kkkkg","ggggx","gggxx","ggxxx","gxxxx","xxxxx","xxxxy","xxxyy","xxyyy","xyyyy","yyyyy","yyyyw","yyyww","yywww","ywwww","wwwww","wwvww","wvvww","vvvww","vvvwz","avvwz","aavwz","aaawz","aaaaz"};
+//        String a = "aaaaa";
+//        String b = "ggggg";
+        String[] arr ={"hot","dot","dog","lot","log","cog"};
+        String a = "hit";
+        String b = "cog";
+        wordLadder2 wl = new wordLadder2();
+        List<List<String>> ladders = wl.findLadders(a, b, Arrays.asList(arr));
+        for(List<String> l: ladders){
+            for (String s :
+                    l) {
+                System.out.print(s+" ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        List<List<String>> ans = new ArrayList<>();
+        Map<String, Set<String>> reverse = new HashMap<>(); // reverse graph start from endWord
+        Set<String> wordSet = new HashSet<>(wordList); // remove the duplicate words
+        wordSet.remove(beginWord); // remove the first word to avoid cycle path
+        Queue<String> queue = new LinkedList<>(); // store current layer nodes
+        queue.add(beginWord); // first layer has only beginWord
+        Set<String> nextLevel = new HashSet<>(); // store nextLayer nodes
+        boolean findEnd = false; // find endWord flag
+        while (!queue.isEmpty()) { // traverse current layer nodes
+            String word = queue.remove();
+            for (String next : wordSet) {
+                if (isLadder(word, next)) { // is ladder words
+                    // construct the reverse graph from endWord
+                    Set<String> reverseLadders = reverse.computeIfAbsent(next, k -> new HashSet<>());
+                    reverseLadders.add(word);
+                    if (endWord.equals(next)) {
+                        findEnd = true;
+                    }
+                    nextLevel.add(next); // store next layer nodes
+                }
+            }
+            if (queue.isEmpty()) { // when current layer is all visited
+                if (findEnd) break; // if find the endWord, then break the while loop
+                queue.addAll(nextLevel); // add next layer nodes to queue
+                wordSet.removeAll(nextLevel); // remove all next layer nodes in wordSet
+                nextLevel.clear();
+            }
+        }
+        if (!findEnd) return ans; // if can't reach endWord from startWord, then return ans.
+        Set<String> path = new LinkedHashSet<>();
+        path.add(endWord);
+        // traverse reverse graph from endWord to beginWord
+        findPath(endWord, beginWord, reverse, ans, path);
+        return ans;
+    }
+
+    private void findPath(String endWord, String beginWord, Map<String, Set<String>> graph,
+                          List<List<String>> ans, Set<String> path) {
+        Set<String> next = graph.get(endWord);
+        if (next == null) return;
+        for (String word : next) {
+            path.add(word);
+            if (beginWord.equals(word)) {
+                List<String> shortestPath = new ArrayList<>(path);
+                Collections.reverse(shortestPath); // reverse words in shortest path
+                ans.add(shortestPath); // add the shortest path to ans.
+            } else {
+                findPath(word, beginWord, graph, ans, path);
+            }
+            path.remove(word);
+        }
+    }
+
+    private boolean isLadder(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int diffCount = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) != t.charAt(i)) diffCount++;
+            if (diffCount > 1) return false;
+        }
+        return diffCount == 1;
+    }
+
+
+}
