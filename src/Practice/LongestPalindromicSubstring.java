a7620561a1e5d46ae1e8ddca659cc7ad9658a9b8
shubham <Shubhamboora10@gmail.com>
2023-05-23 12:00:58 +0530

    LongestPalindromicSubstring & ZigzagConversion code

---
+++ src/Practice/LongestPalindromicSubstring.java
@@ -0,0 +1,38 @@
+package Practice;
+
+import java.util.Objects;
+
+public class LongestPalindromicSubstring {
+
+    public static void main(String[] args) {
+        System.out.println(LongestPalindromicSubstring.longestPalindrome("s"));
+    }
+
+    public static String longestPalindrome(String s){
+        if (s.length()<=0)return "You fucked up.";
+        int low,high,len = 1, index=0, start=0;
+        while(index < s.length()){
+            low = index-1; high=index+1;
+
+            //check for same alphabets on left side
+            while(low>=0 && s.charAt(low) == s.charAt(index))low--;
+
+            //check for same alphabets on right side
+            while(high<s.length() && s.charAt(high) == s.charAt(index))high++;
+
+            //ittrating until left and right alphabets are same
+            while(low>=0 && high <s.length() && s.charAt(low) == s.charAt(high)){
+                low--; high++;
+            }
+
+            int size = high - low -1;
+            if (size>len){
+                len = size;
+                start = low+1;
+            }
+            index++;
+        }
+        System.out.println("len = "+ len);
+        return s.substring(start, start+len);
+    }
+}
---
+++ src/Practice/ZigzagConversion.java
@@ -0,0 +1,54 @@
+package Practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZigzagConversion {
+
+    public static void main(String[] args) {
+        System.out.println(ZigzagConversion.convert("PAYPALISHIRING", 3));
+    }
+
+    public static String convert(String s, int numRows) {
+
+        List<StringBuilder> list = new ArrayList<>();
+        int size = Math.min(numRows, s.length());
+
+        //Initializing StringBuilder into list acc to numRows
+        for (int i=0;i<size;i++)list.add(new StringBuilder());
+
+        boolean movingDown = true;
+        list.get(0).append(s.charAt(0));
+        int builder = 1;
+
+
+        //Result: PAHNAPLSIIGYIR
+        for (int i=1; i<s.length();i++){
+            char ch = s.charAt(i);
+            list.get(builder).append(ch);
+
+            //If working on last stringBuilder of list movement need to be reversed.
+            if (builder == size-1) {
+                builder--;
+                movingDown = false;
+                continue;
+            }
+            //If working on first stringBuilder of list movement need to be reversed.
+            if (builder == 0) {
+                builder++;
+                movingDown = true;
+                continue;
+            }
+            //++, -- based upon movement direction.
+            builder += movingDown?1:-1;
+
+        }
+
+        StringBuilder result = new StringBuilder();
+        list.forEach(str-> result.append(str));
+
+        return result.toString();
+    }
+
+
+}
