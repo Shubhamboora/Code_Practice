db3938a0688724dbab1489b92b2cab1f9436385e
shubham <Shubhamboora10@gmail.com>
2023-05-25 01:13:25 +0530

    Stock saprator

---
+++ src/Sapretstocks.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Sapretstocks {
+
+    public static void main(String[] args) {
+
+        Arrays.stream(Sapretstocks.arr).forEach(a->{
+            if (a.matches("^[a-zA-Z]+$")) System.out.print(a+",");
+        });
+    }
+
+    static String arr[] = new String[]{};
+}
